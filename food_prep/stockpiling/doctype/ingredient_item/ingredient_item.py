b4188d11511fed7d35b2d17d8be4d6ebdcf2bd34
Abadulrehman <Abadulrehman@gmail.com>
2022-07-14 08:42:34 +0400

    feat: introuduce basic doctypes

---
+++ food_prep/stockpiling/doctype/ingredient_item/ingredient_item.py
@@ -0,0 +1,13 @@
+# Copyright (c) 2022, Abdulrehman Muhammad Younis and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class IngredientItem(Document):
+
+    # TODO:
+    # Copy unit from ingredient Recipe
+
+    pass
